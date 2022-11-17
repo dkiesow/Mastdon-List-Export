726acd67676a1a3fc917c29277241db9829e28c2
Damon Kiesow <damon@kiesow.net>
2022-11-17 14:33:54 -0600

    Create config.py
    Created config file

---
+++ config.py
@@ -0,0 +1,7 @@
+settings_remote = {
+    "endpoint": "https://mastodon.example/api/v1/",
+    "token": "token from create app within Mastodon admin",
+    "list": "ID#",
+    "owner": "your @address if you want in in the list",
+    "path": "/path/to/save/the/file"
+}
