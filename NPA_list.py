ceff124448173d4f362654eb86f76f8d85aeb2e4
Damon Kiesow <damon@kiesow.net>
2022-11-17 14:21:45 -0600

    Add files via upload
    Using Mastodon API to access the members of a private list and exporting them to a CSV.

---
+++ NPA_list.py
@@ -0,0 +1,37 @@
+import requests
+import json
+import csv
+import config as cfg
+
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
+
+settings = cfg.settings_local
+#settings = cfg.settings_remote
+endpoint = settings['endpoint']
+token = settings['token']
+path = settings['path']
+owner = settings['owner']
+
+response = requests.get(endpoint+"lists/3/accounts", auth=BearerAuth(token))
+follow = response.json()
+
+to_follow={}
+for x in range(len(follow)):
+    to_follow[x] = follow[x]['acct']
+y=len(to_follow)
+to_follow[y] = owner
+
+cols = ['account']
+with open(path, 'w', newline='') as csvfile:
+    fieldnames = ['Account Address']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for key in to_follow:
+        writer.writerow({'Account Address': to_follow[key]})
+
